3a727011387d39802aeed629bf347f2bc8d288bc
Kullanıcı Adınız <email@adresiniz.com>
2023-03-27 23:17:52 +0300

    birinci hafta ödevi yorum satırları ekşlendi

--- App.js
+++ App.js
@@ -1,2 +1,4 @@
+//index.js sayfasindaki fonksiyonu içe aktarma
 import getData from "./index.js";
+//fonksiyonu çalıştirma işlemşnşn yapılması
 getData(1);
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+//projete axios'u dahil etme
 import axios from "axios";
 async function getData(userId) {
+  //axios ile API dan veriyi çekme işleminin yapılması ve data olarak gelen değişkenin user olarak değiştirip içinde atama
   const { data: user } = await axios(
     `https://jsonplaceholder.typicode.com/users/${userId}`
   );
+  //axios ile API dan veriyi çekme işleminin yapılması ve data olarak gelen değişkenin post olarak değiştirip içinde atama
+
   const { data: post } = await axios(
     `https://jsonplaceholder.typicode.com/posts/${userId}`
   );
-
+  //gelen veriyi ekrana yazdırma
   console.log("user", user);
   console.log("post", post);
 }
+//fonksiyonu dışa aktarma
 export default getData;
